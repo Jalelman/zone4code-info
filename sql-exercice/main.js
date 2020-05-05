ea07def56dce702e8e2325c9be408800ed57f961
jobran41 <jobran41@gmail.com>
2020-05-05 14:39:29 +0100

    Add files via upload

---
+++ sql-exercice/main.js
@@ -0,0 +1,57 @@
+
+/**
+ * Question 1:
+ * create you DATABASE name zone-test.
+ */
+
+// your answer here 
+
+/**
+ * Question 2:
+ * create your TABLES.
+ */
+
+// your answer here 
+	 
+/**
+ * Question 3:
+ * List the first name, last name, and city of all customers.
+ */
+
+// your answer here 
+
+
+/**
+ * Question 4:
+ * Add a record for a new customer (hamza mhadbi and jobran amairi) and Country to tunis
+ */
+
+ // your answer here 
+
+
+
+
+/**
+ * Question 5:
+ * Discontinue products over $50.
+ */
+
+ // your answer here 
+
+
+ /**
+ * Question 5:
+ * List all products with names that start with 'Ca'
+ */
+
+ // your answer here 
+
+
+  /**
+ * Question 6:
+ *  List the number of customers in each country
+ */
+
+ // your answer here 
+
+
